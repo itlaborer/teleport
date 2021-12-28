e811355cb13ade10df8677e542b1f37c4af1b10a
Zac Bergquist <zac.bergquist@goteleport.com>
2021-12-28 15:03:54 -0700

    Record desktop sessions
    Here we introduce a new protobuf type (DesktopRecording) that contains
    an encoded TDP message, and update AuditWriter to treat these similarly
    to SessionPrint events (which are used for SSH session recordings).

--- api/types/events/oneof.go
+++ api/types/events/oneof.go
@@ -293,6 +293,10 @@ func ToOneOf(in AuditEvent) (*OneOf, error) {
 		out.Event = &OneOf_WindowsDesktopSessionEnd{
 			WindowsDesktopSessionEnd: e,
 		}
+	case *DesktopRecording:
+		out.Event = &OneOf_DesktopRecording{
+			DesktopRecording: e,
+		}
 	default:
 		return nil, trace.BadParameter("event type %T is not supported", in)
 	}
@@ -430,6 +434,8 @@ func FromOneOf(in OneOf) (AuditEvent, error) {
 	} else if e := in.GetWindowsDesktopSessionStart(); e != nil {
 		return e, nil
 	} else if e := in.GetWindowsDesktopSessionEnd(); e != nil {
+		return e, nil
+	} else if e := in.GetDesktopRecording(); e != nil {
 		return e, nil
 	} else {
 		if in.Event == nil {
